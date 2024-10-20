b1bbc485e7d0a34c31ee774092a351ca34a61624
Leo BOURBON <bourbon.leo@hotmail.fr>
2024-10-20 23:03:43 +0200

    astro-robots-txt integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://leo-bourbon.github.io",
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), robotsTxt()],
 });
