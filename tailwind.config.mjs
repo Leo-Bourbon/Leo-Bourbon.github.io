49414022d24df58782e97b1db07eb87465efe68e
Leo BOURBON <bourbon.leo@hotmail.fr>
2024-10-23 01:21:49 +0200

    DaisyUI and tailwindcss/typography, tailwindcss-animate plugins added
    tailwindcss-animate plugin was also patched to rename the "duration" property to "animation-duration", in order to fix a bug when using custom durations.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography";
+import animate from "tailwindcss-animate";
+import daisyui from "daisyui";
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [typography, animate, daisyui],
 }
